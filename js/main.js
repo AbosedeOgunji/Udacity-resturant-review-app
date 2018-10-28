276db1bdcc24616e47fb94abc30610c9f09fd2a5
Abosede Ogunji <abosedeogunji@gmail.com>
2018-10-28 13:25:32 +0100

    header changed
    line 167 header was changed from h1 to h2, also line 180 more.innerHTML = ' was given a better description'

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ createRestaurantHTML = (restaurant) => {
   image.alt = restaurant.name + " Restaurant";
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -177,7 +177,7 @@ createRestaurantHTML = (restaurant) => {
   li.append(address);
 
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View Details of the restaurant Emily';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
